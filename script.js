6c18f3edf8ca9af7047b7fe5f7249e9f688080fe
GrgSimion <grgsimion@gmail.com>
2024-03-04 19:06:46 +0200

    add const

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let years = 5;
 console.log(age);
 console.log(years);
 
+const piNumber = 3.14;
+console.log(piNumber);
 let name = "George";
 console.log(name);
+
+// operatori date numerice
+
 
 
 
