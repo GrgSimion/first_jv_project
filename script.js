9af5b87c35ed293fae6d6fcb45409057d86c88f7
GrgSimion <grgsimion@gmail.com>
2024-03-04 19:23:54 +0200

    adunare si scadere

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ console.log(name);
 // operatori date numerice
 
 
+// adunare
+let firstNumber = 10;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
+
+// scadere
+let pastAge = age - years;
+console.log(`Acum ${years} ani avem varsta de ${pastAge}`);
+console.log(`Acum ${years} ani aveam varsta de ${age-years}`);
 
 
